411ae29626b4a2f56099df84f88571de8796cad1
Fernando Choquez <javierchoquez1608@gmail.com>
2025-08-31 14:03:06 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import matplotlib
+import csv
+from matplotlib import pyplot as plt
+
+# Visualización de datos para linux, si eres de windows busca la documentación correspondiente
+matplotlib.use("TkAgg")
+
+# Abrimos y almacenamos los datos
+X_axis = [] # Año
+Y_axis = [] # Hijos por mujer
+
+# Datos extraidos de: https://datos.bancomundial.org/indicador/SP.DYN.TFRT.IN?end=2023&locations=PE&start=1960&view=chart
+with open("./peru.csv") as data:
+    csv_reader = csv.reader(data)
+
+    for year in next(csv_reader):
+        X_axis.append(int(year))
+
+    for n in next(csv_reader):
+        Y_axis.append(float(n))
+
+print(X_axis)
+print(Y_axis)
+
+# Creamos un grafico de barras
+plt.bar(X_axis, Y_axis)
+
+plt.xlabel("Año")
+plt.ylabel("Hijos por mujer")
+
+plt.show()
